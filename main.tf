1573179054f6fa18b3c5a84a98465ed71647d5c3
kirillsubin <kirillsubin12@gmail.com>
2020-06-01 14:18:42 +0300

    First commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  credentials = "${file("${var.credentials}")}"
+  project = "${var.project}"
+  region = "${var.region}"
+  zone = "${var.zone}"
+}
+resource "google_compute_instance" "vm_instance1" {
+ name = "${var.instance-name-1}"
+ machine_type = "${var.machine-type-1}"
+ boot_disk {
+   initialize_params {
+     image = "${var.image-1}"
+   }
+ }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+} 
